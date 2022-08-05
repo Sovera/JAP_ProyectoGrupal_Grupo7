ca4fd782d551119a6f47b5655cf29bff62bbf075
Sovera <tiagoromanvera@gmail.com>
2022-08-05 15:36:49 -0300

    Eliminar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let url="https://crudcrud.com/api/68d64a2d84b54494834c2fca73f702ba/grupo7"
 function mostrar(){
     alert ("este es el superlistado");
+}
+
+function eliminar(){
+    delete Employee.firstname;
 }
